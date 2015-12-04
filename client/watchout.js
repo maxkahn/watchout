97519ded2daa2f98e1bbbcf15716a3badb17fdfa
maxkahn <kahn.max@gmail.com>
2015-12-04 15:47:25 -0800

    Drew enemies in an svg element

--- client/watchout.js
+++ client/watchout.js
@@ -8,8 +8,20 @@ var canvas = d3.selectAll("body")
 
 //create circle
 	//make into class
-var circle = canvas.append("circle")
-					.attr("cx", 100)
-					.attr("cy", 100)
-					.attr("r", 75)
-					.attr("fill", "red");
+// var circle = canvas.append("circle")
+// 					.attr("cx", 100)
+// 					.attr("cy", 100)
+// 					.attr("r", 75)
+// 					.attr("fill", "red")
+// 					.attr("class", "enemy");
+
+var enemies = canvas.selectAll('enemies')
+					.data([50,100,300])
+					.enter()
+					.append('circle')
+					.attr("cx", function(d){ return d + 100})
+					.attr("cy", function(d){ return d + 100})
+					.attr("r", 13)
+					.attr("fill", "red")
+					.attr("class", "enemies");
+					
