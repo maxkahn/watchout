40c55542025bfe3723c77c023e1d4451ba2d11a4
maxkahn <kahn.max@gmail.com>
2015-12-04 15:21:38 -0800

    We made a circle!!!

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,15 @@
 // start slingin' some d3 here.
+
+//create canvas, then attach elements
+var canvas = d3.selectAll("body")
+				.append("svg")
+				.attr("height", 500)
+				.attr("width", 500);
+
+//create circle
+	//make into class
+var circle = canvas.append("circle")
+					.attr("cx", 100)
+					.attr("cy", 100)
+					.attr("r", 75)
+					.attr("fill", "red");
